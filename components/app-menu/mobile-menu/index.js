4e72066bf694a911a9f6169ae8d9333c3ad510b2
narekdanielyan19 <narekdanielyan@atavit.com>
2023-07-07 17:54:55 +0400

    fix navbar styles

--- components/app-menu/mobile-menu/index.js
+++ components/app-menu/mobile-menu/index.js
@@ -1,11 +1,14 @@
 import { Menu, MenuItem } from '@mui/material';
 import { StyledButton, StyledLink } from 'components/Layout/main-header/style';
 import { IconButton } from 'components/button/icon-button';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useState } from 'react';
 import { StyledMobileMenu } from './style';
 
 export const MobileMenu = ({ onLogout }) => {
+    const { data: session } = useSession();
+
     const [anchorEl, setAnchorEl] = useState(null);
 
     const handleClick = (event) => {
@@ -18,41 +21,58 @@ export const MobileMenu = ({ onLogout }) => {
 
     return (
         <StyledMobileMenu>
-            <IconButton
-                iconName="menu"
-                onClick={handleClick}
-                tooltipText="Menu"
-            />
-            <Menu
-                anchorEl={anchorEl}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                onClick={handleClose}
-                onClose={handleClose}
-                open={!!anchorEl}
-                PaperProps={{
-                    elevation: 0,
-                    sx: {
-                        overflow: 'visible',
-                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                        mt: 1.5,
-                    },
-                }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-            >
-                <MenuItem>
-                    <Link href="/" passHref>
-                        <StyledLink>NEXT EVENTS</StyledLink>
-                    </Link>
-                </MenuItem>
-                <MenuItem>
-                    <Link href="/events" passHref>
-                        <StyledLink>All events</StyledLink>
-                    </Link>
-                </MenuItem>
-                <MenuItem>
-                    <StyledButton onClick={onLogout}>Log out</StyledButton>
-                </MenuItem>
-            </Menu>
+            {session ? (
+                <>
+                    <IconButton
+                        iconName="menu"
+                        onClick={handleClick}
+                        tooltipText="Menu"
+                    />
+                    <Menu
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            horizontal: 'right',
+                            vertical: 'bottom',
+                        }}
+                        onClick={handleClose}
+                        onClose={handleClose}
+                        open={!!anchorEl}
+                        PaperProps={{
+                            elevation: 0,
+                            sx: {
+                                overflow: 'visible',
+                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                mt: 1.5,
+                            },
+                        }}
+                        transformOrigin={{
+                            horizontal: 'right',
+                            vertical: 'top',
+                        }}
+                    >
+                        <MenuItem>
+                            <Link href="/" passHref>
+                                <StyledLink>NEXT EVENTS</StyledLink>
+                            </Link>
+                        </MenuItem>
+                        <MenuItem>
+                            <Link href="/events" passHref>
+                                <StyledLink>All events</StyledLink>
+                            </Link>
+                        </MenuItem>
+                        <MenuItem>
+                            <StyledButton onClick={onLogout}>
+                                Log out
+                            </StyledButton>
+                        </MenuItem>
+                    </Menu>
+                </>
+            ) : (
+                <Link href="/auth" passHref>
+                    <StyledLink>Log in</StyledLink>
+                </Link>
+            )}
+            ;
         </StyledMobileMenu>
     );
 };
